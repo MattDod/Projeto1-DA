07f78caee094b024bdc76c3c0c3dd35c97985e5a
Luís Cordeiro <kammeratlego14@gmail.com>
2024-03-19 14:44:31 +0000

    Primeiro Commit

---
+++ src/Menu.h
@@ -0,0 +1,15 @@
+//
+// Created by kamert on 19-03-2024.
+//
+
+#ifndef PROJETODA_MENU_H
+#define PROJETODA_MENU_H
+
+#include "System.h"
+
+class Menu{
+public:
+
+    void start(System system);
+};
+#endif //PROJETODA_MENU_H
---
+++ src/System.h
@@ -0,0 +1,13 @@
+//
+// Created by kamert on 19-03-2024.
+//
+
+#ifndef PROJETODA_SYSTEM_H
+#define PROJETODA_SYSTEM_H
+using namespace std;
+class System{
+public:
+    System();
+
+};
+#endif //PROJETODA_SYSTEM_H
